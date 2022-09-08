f82702a16fec53544f4cf6167d0b65ea6e9a6302
Saulo Freire <gbsaulo@gmail.com>
2022-09-07 23:20:42 -0300

    feat: lan configuration struct

--- Sources/Salve/Salve.swift
+++ Sources/Salve/Salve.swift
@@ -8,29 +8,36 @@ public struct Salve {
     }
 }
 
+public struct LANConfiguration {
+    let serviceTypeName: String
+}
+
 public class LAN: NSObject, MCBrowserViewControllerDelegate {
 
     public static let shared = LAN()
     var mcSession: MCSession!
     var mcAdvertiserAssistant: MCNearbyServiceAdvertiser!
     let peerID = MCPeerID(displayName: UIDevice.current.name)
-    var serviceTypeName = "salve-connect"
     var mcAdvertiserDelegate: LANAdvertiserDelegate!
+    var configuration: LANConfiguration!
     
     private override init() {
         super.init()
         self.mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
         self.mcAdvertiserDelegate = LANAdvertiserDelegate(session: mcSession)
+        let bundle = Bundle.main
+        let device = UIDevice.current
+        self.configuration = LANConfiguration(serviceTypeName: "\(bundle.bundleIdentifier ?? device.name).lan")
     }
     
     public func hostSession() {
-        mcAdvertiserAssistant = MCNearbyServiceAdvertiser(peer: peerID, discoveryInfo: nil, serviceType: serviceTypeName)
+        mcAdvertiserAssistant = MCNearbyServiceAdvertiser(peer: peerID, discoveryInfo: nil, serviceType: configuration.serviceTypeName)
         mcAdvertiserAssistant.delegate = mcAdvertiserDelegate
         mcAdvertiserAssistant.startAdvertisingPeer()
     }
     
     public func joinHostedSession(fromViewController viewController: UIViewController, animated: Bool) {
-        let browser = LANMCBrowserFactory.make(withServiceType: serviceTypeName, session: mcSession)
+        let browser = LANMCBrowserFactory.make(withServiceType: configuration.serviceTypeName, session: mcSession)
         browser.delegate = self
         viewController.present(browser, animated: animated)
     }
